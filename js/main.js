e2b4452cab7834118a0bf30832136f6accdd2327
Trevor Van Rys <trevor_newmacbook@New-MacBook-Pro.local>
2022-09-09 10:48:37 -0400

    workin with JS

---
+++ js/main.js
@@ -0,0 +1,26 @@
+(() => {
+    const lbLinks = document.querySelectorAll('a:not(.close)');
+
+    let theCaptions = [
+        {
+            name: "example1",
+            copy: "This is some expample text for the first link"
+        },
+
+        {
+            name: "example2",
+            copy: "This is some expample text for the second link"
+        }
+    ]
+
+    function popLightbox() {
+        debugger;
+        let currentCaption = document.querySelector('.lightbox figcaption'),
+            currentName = this.dataset.copyref,
+            theCopy = theCaptions.filter(item => item.name == currentName);
+
+        currentCaption.textContent = theCopy[0].copy;
+    }
+
+    lbLinks.forEach(link => link.addEventListener('click', popLightbox));
+})();
